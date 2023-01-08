cd9b63f86d0e7119f953d1ee3c4fa5dbe0562808
SJTU-MI <95070809+SJTU-MI@users.noreply.github.com>
2023-01-08 09:38:36 +0800

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,10 @@
+# download.sh is used to
+# get some large files (>25M)
+# get POLYINFO_PI1M.pkl
+mv POLYINFO_PI1M.pkl ./Data02-different_descriptors/graph_descriptor/POLYINFO_PI1M.pkl
+# embed_PI1M.pkl
+mv embed_PI1M.pkl ./Data02-different_descriptors/Similarity_Analysis/embed_PI1M.pkl
+# get RF.model
+mv RF.model ./Data03-ML_models/RF.model
+# get datasteB.pkl (PI1M)
+mv datasteB.pkl ./Data05-Polymer_data/datasteB.pkl
